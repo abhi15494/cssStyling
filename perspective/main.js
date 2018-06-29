ea8a25eea0c0c3b26084301c46e69bdfafad2eb4
abhi15494 <abhishek15494@gmail.com>
2018-06-29 14:00:54 +0530

    updates

--- perspective/main.js
+++ perspective/main.js
@@ -2,7 +2,7 @@ cube = document.querySelector(".grand-parent .parent");
 let valX = 0;
 let valY = 0;
 
-setInterval(()=>{
+var animate = setInterval(()=>{
     cube.style.transform=`rotateX(${valX}deg) rotateY(${valY}deg)`;
     valX+=5;
     valY+=5;
@@ -10,4 +10,10 @@ setInterval(()=>{
     //     valX = 0;
     //     valY = 0;
     // }
+    console.clear();
+    console.log(valX);
+    console.log(valY);
 }, 50);
+
+console.log(valX);
+console.log(valY);
